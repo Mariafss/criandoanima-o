99d6cb23f83e84cd95018ea77387f74d0dfc7375
Mariafss <141141462+Mariafss@users.noreply.github.com>
2024-03-04 10:25:53 -0300

    m

---
+++ sketch.js
@@ -0,0 +1,44 @@
+let cor;
+let posicaoHorizontal; // x
+let posicaoVertical; // y
+
+function setup() {
+  createCanvas(400, 400);
+  background('#03A9F4');
+  cor = color(random(0, 255), random(0, 255), random(0, 255));
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
+}
+
+function draw() {
+  circle(30, 30, 20);
+}
+
+function draw() {
+  fill('purple');
+  circle(posicaoHorizontal, posicaoVertical, 50);
+  
+  if(mouseX < posicaoHorizontal) {
+posicaoHorizontal--;
+  
+  }
+  
+  if(mouseX > posicaoHorizontal) {
+posicaoHorizontal++;
+  }
+  
+  if(mouseY < posicaoVertical) {
+posicaoVertical--;
+    
+  }
+  
+  if(mouseY > posicaoVertical) {
+posicaoVertical++;
+  
+  } 
+  
+  if(mouseIsPressed) {
+cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+  }
+  
+}
